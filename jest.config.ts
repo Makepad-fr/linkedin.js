956aa34ca47841580b035cd06d126137f478a54d
kaanyagci <kaan.yagci@makepad.fr>
2023-05-28 14:26:28 +0200

    tests: tests added for login flow

--- jest.config.ts
+++ jest.config.ts
@@ -76,16 +76,7 @@ export default {
   // ],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "mjs",
-  //   "cjs",
-  //   "jsx",
-  //   "ts",
-  //   "tsx",
-  //   "json",
-  //   "node"
-  // ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
@@ -100,7 +91,7 @@ export default {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
+  preset: 'ts-jest',
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -144,7 +135,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-node",
+  testEnvironment: "node",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -173,8 +164,9 @@ export default {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
-
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
   //   "/node_modules/",
---
+++ tests/linkedin.test.ts
@@ -0,0 +1,53 @@
+// TODO: Login and check if the browser context saved
+/* TODO: Login and check if user logged in
+    * Create browser context with the first login
+    * Login again using the existing context
+    * Check if the user logged in
+*/
+import LinkedIn from "../src";
+import fs from 'fs';
+   
+
+
+
+describe('LinkedIn module', () => {
+    let contextFilePath: string;
+    let linkedIn: LinkedIn;
+    
+    let username: string;
+    let password: string;
+
+    beforeAll(() => {
+        const u = process.env['LINKEDIN_USERNAME'];
+        if (u === undefined) {
+            throw new Error("LINKEDIN_USERNAME environment variable is missing");
+        }
+        const p = process.env['LINKEDIN_PASSWORD'];
+        if (p === undefined) {
+            throw new Error('LINKEDIN_PASSWORD environment variable is missing');
+        }
+        username = u;
+        password = p;
+    })
+
+    beforeEach(async () => {
+        contextFilePath = `./${crypto.randomUUID()}.json`;
+        linkedIn = await LinkedIn.init({headless: true, contextPath: contextFilePath});
+    })
+
+    test('Check if login works', async () => {
+        // Logging in to LinkedIn account
+        await linkedIn.login(username, password);
+        // Verify if the context path exists
+        expect(fs.existsSync(contextFilePath)).toBeTruthy();
+        // Verify if the user logged in
+        expect(await linkedIn.isUserLoggedIn()).toBeTruthy();
+    }, 600000);
+
+    afterEach(async () => {
+        if (fs.existsSync(contextFilePath)) {
+            fs.rmSync(contextFilePath);
+        }
+        await linkedIn.close();
+    })
+  });
