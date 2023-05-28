f0193653cbb509dd92691bb4e9d22513b610bfd0
kaanyagci <kaan.yagci@makepad.fr>
2023-05-28 14:26:28 +0200

    feat: example updated to check if the user logged in

--- src/example.ts
+++ src/example.ts
@@ -6,9 +6,11 @@ const AUTHENTICATION_CONTEXT_PATH = "./linkedin.json";
 
 async function main() {
 
-    const linkedIn = await LinkedIn.init({headless: false, contextPath: AUTHENTICATION_CONTEXT_PATH});
+    const linkedIn = await LinkedIn.init({headless: true, contextPath: AUTHENTICATION_CONTEXT_PATH});
     // Login to LinkedIn account
     await linkedIn.login(LINKEDIN_USERNAME!, LINKEDIN_PASSWORD!);
+    console.log('Is user logged in');
+    console.log(await linkedIn.isUserLoggedIn());
 }
 
 main().then();
