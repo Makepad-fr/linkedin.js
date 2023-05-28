45f9ea45378d8bedb4f24b35360581c68a76a11b
kaanyagci <kaan.yagci@makepad.fr>
2023-05-29 01:26:33 +0200

    refactor: url constants moved to the url-factory

---
+++ src/url-factory.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = 'https://www.linkedin.com/';
+export const LOGIN_URL = `${BASE_URL}login/`
+export const FEED_URL = `${BASE_URL}feed/`
+export const PROFILE_BASE_URL = `${BASE_URL}in/`;
